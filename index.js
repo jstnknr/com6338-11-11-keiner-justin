794b768330e425a822cc5e729051c2d7af57ab14
jstnknr <143372221+jstnknr@users.noreply.github.com>
2024-04-21 23:38:01 -0400

    all good

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const getPoemBtn = document.getElementById('get-poem')
-const poemEl = document.getElementById('poem')
-const poemURL = 'https://poetrydb.org/random,linecount/1;12/author,title,lines.json'
+const getPoemBtn = document.getElementById('get-poem');
+const poemEl = document.getElementById('poem');
+const poemURL = 'https://poetrydb.org/random,linecount/1;12/author,title,lines.json';
 
-const getJSON = url => fetch(url).then(res => res.json())
+const getJSON = url => fetch(url).then(res => res.json());
 
-const pipe = (...fns) => firstArg => fns.reduce((returnValue, fn) => fn(returnValue), firstArg)
-
-const makeTag = tag => str => `<${tag}>${str}</${tag}>`
+const makeTag = tag => str => `<${tag}>${str}</${tag}>`;
 
 const makePoemHTML = poem => {
   const titleTag = makeTag('h2');
+  const authorTag = makeTag('h3');
   const lineTag = makeTag('p');
 
   const title = titleTag(poem.title);
+  const author = authorTag(poem.author);
   const lines = poem.lines.map(line => lineTag(line)).join('');
 
-  return `${title}${lines}`;
+  return `${title}${author}${lines}`;
 }
 
 getPoemBtn.onclick = async function() {
   try {
     const response = await getJSON(poemURL);
-    const poem = response[0]; // Assuming the response is an array of poems, we take the first one.
+    const poem = response[0]; 
     poemEl.innerHTML = makePoemHTML(poem);
   } catch (error) {
     console.error('Error fetching poem:', error);
-    poemEl.innerHTML = 'Error fetching poem. Please try again later.';
+    poemEl.innerHTML = 'Sorry, an error occured fetching a poem. Please try again later.';
   }
 }
